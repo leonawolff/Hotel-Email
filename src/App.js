c7c19f0d2f8371e879eca4377bbcc89ab4b2f1dc
EllenWhelan <whelane5@tcd.ie>
2020-04-14 11:29:13 +0100

    Email working!

--- src/App.js
+++ src/App.js
@@ -32,10 +32,14 @@ import "./App.css";
 export default function App() {
 function sendEmail(e){
     e.preventDefault();
-    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID,document.forms["my-form"],process.env.REACT_APP_USER_ID)
+    console.log(process.env.REACT_APP_SERVICE_ID);
+    console.log(process.env.REACT_APP_TEMPLATE_ID);
+    console.log(process.env.REACT_APP_USER_ID);
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID,e.target,process.env.REACT_APP_USER_ID)
     .then((result) => {
         console.log(result.text);
     }, (error) => {
+
         console.log(error.text);
     });
   }
